cb300204922a0c166d1dd839fca1c19b28d4f554
Jiri Vit <jiri.vit@windowslive.com>
2015-12-07 22:32:57 +0100

    bin2c: support more constants in one row

--- python/bin2c.py
+++ python/bin2c.py
@@ -1,13 +1,22 @@
 infname = 'fem.raw'
 outfname = 'fem.c'
+rowlen = 10
 
 infile = open(infname, 'rb')
 outfile = open(outfname, 'w')
 
+col = 0
+
 bstr = infile.read(2)
 while (len(bstr) > 0):
 	data = bstr[0] + (bstr[1] << 8)
-	outfile.write(str.format('0x{0:04x},\n', data))
+	outfile.write(str.format('0x{0:04x},', data))
+	col = col + 1
+	if (col >= rowlen):
+		outfile.write('\n')
+		col = 0
+	else:
+		outfile.write(' ')
 	bstr = infile.read(2)
 
 infile.close()
