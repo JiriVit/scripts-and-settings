5334984883fbdb55c406723cd5bd7ce14135496c
Jiri Vit <vit@jablocom.com>
2015-12-11 17:39:40 +0100

    bin2c: some modifications to make the script more universal

--- python/bin2c.py
+++ python/bin2c.py
@@ -1,25 +1,41 @@
-infname = 'fem.raw'
-outfname = 'fem.c'
-rowlen = 10
+import sys
+import getopt
 
-infile = open(infname, 'rb')
-outfile = open(outfname, 'w')
-
-col = 0
-
-bstr = infile.read(2)
-while (len(bstr) > 0):
-	data = bstr[0] + (bstr[1] << 8)
-	outfile.write(str.format('0x{0:04x},', data))
-	col = col + 1
-	if (col >= rowlen):
-		outfile.write('\n')
-		col = 0
-	else:
-		outfile.write(' ')
+def main(argv):
+	infname = 'fem.raw'
+	outfname = 'fem.c'
+	rowlen = 10
+	datalen = 2
+	signed = 1
+	base = 16
+	
+	infile = open(infname, 'rb')
+	outfile = open(outfname, 'w')
+	
+	col = 0
+	
 	bstr = infile.read(2)
+	while (len(bstr) > 0):
+		data = bstr[0] + (bstr[1] << 8)
+		if (base == 16):
+			outfile.write(str.format('0x{0:04x},', data))
+		if (base == 10):
+			if (signed == 1):
+				if (data > pow(2, datalen * 8 - 1)):
+					data = data - pow(2, datalen * 8)
+			outfile.write(str.format('{0},', data))
+		col = col + 1
+		if (col >= rowlen):
+			outfile.write('\n')
+			col = 0
+		else:
+			outfile.write(' ')
+		bstr = infile.read(2)
+	
+	infile.close()
+	outfile.close()
 
-infile.close()
-outfile.close()
+if __name__ == "__main__":
+	main(sys.argv[1:])
 
 # vim: set ft=python ts=4 sts=4 sw=4 ai noexpandtab :
