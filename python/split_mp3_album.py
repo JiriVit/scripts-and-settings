6361e6cd15207e0ec3f6dbd95f5a508e032b3e33
Jiří Vít <jiri.vit@windowslive.com>
2024-01-17 19:51:08 +0100

    split_mp3_album.py: split the code to functions

--- python/split_mp3_album.py
+++ python/split_mp3_album.py
@@ -13,12 +13,9 @@ Pre-requisites:
 - mutagen
 
 Recommended YouTube to MP3 Converter:
-y2mate.is
+y2mate.com
 
-TODO Add writing of artist, album and cover art to the ID3 tag. This will require use of an input
-     definition file (because command line doesn't support UTF-8 characters) and standard ID3 class
-     (instead of EasyID3 which doesn't support cover art).
-TODO Add autodetection of tracklist format ... or interactive selection.
+TODO Add use of XML definition file.
 TODO Smoothen the use of artist in the new format, there is room for improvement.
 """
 
@@ -26,13 +23,18 @@ import glob
 import os
 import re
 import sys
+import xml.etree.cElementTree as ET
 
 import ffmpeg
 from mutagen.easyid3 import EasyID3
 
+#---------------------------------------------------------------------------------------------------
+# Constants
+#---------------------------------------------------------------------------------------------------
+
 # debug options
 SKIP_EXISTING_TRACKS = False
-STOP_AFTER_X_TRACKS = None
+STOP_AFTER_X_TRACKS = 1
 
 TRACK_FORMATS = [
     ('00:00 Title', r'([\d:]+)\s+(.+)', ['start_time', 'title']),
@@ -41,99 +43,125 @@ TRACK_FORMATS = [
     ('00:00 Artist - Title', r'([\d:]+)\s+(.+)\s-\s(.+)', ['start_time', 'artist', 'title']),
 ]
 
-# process command-line arguments
-if len(sys.argv) == 3:
-    album_mp3_path = sys.argv[1]
-    tracklist_path = sys.argv[2]
-else:
-    mp3_list = glob.glob('*.mp3')
-    if len(mp3_list) > 0:
-        album_mp3_path = mp3_list[0]
-        print(f"WARNING: No MP3 file specified, using '{album_mp3_path}'.")
+#---------------------------------------------------------------------------------------------------
+# Functions
+#---------------------------------------------------------------------------------------------------
+
+def get_input_filenames():
+    if len(sys.argv) == 3:
+        album_mp3_path = sys.argv[1]
+        tracklist_path = sys.argv[2]
     else:
-        print('ERROR: No MP3 file specified, none found.')
-        sys.exit(1)
-    txt_list = glob.glob('*.txt')
-    if len(txt_list) > 0:
-        tracklist_path = txt_list[0]
-        print(f"WARNING: No tracklist specified, using '{tracklist_path}'")
-    else:
-        print('ERROR: No tracklist specified, none found.')
-        sys.exit(1)
-
-# read lines from tracklist text file
-with open(tracklist_path, "rt", encoding="utf8") as fobj:
-    lines = fobj.readlines()
-
-# ask for selection of tracklist format
-print(f'\nFirst line of provided tracklist:')
-print(lines[0])
-print('Supported tracklist formats:')
-trfl_len = len(TRACK_FORMATS)
-for i in range(trfl_len):
-    trf = TRACK_FORMATS[i]
-    print(f'{i + 1}:   {trf[0]}')
-ans = int(input('Select format (1-4): '))
-selected_trf = TRACK_FORMATS[int(ans) - 1]
-
-# parse the line with a regex, to get track info
-trf_regex = selected_trf[1]
-trf_fields = selected_trf[2]
-regex = re.compile(trf_regex)
-tracklist = []
-for line in lines:
-    match = regex.match(line)
-    track_info = {}
-    for i in range(len(trf_fields)):
-        track_info[trf_fields[i]] = match.group(i+1)
-    tracklist.append(track_info)
-
-# ask for confirmation of parsed tracklist
-print("Tracklist has been parsed to following items:")
-print('\n'.join([str(x) for x in tracklist]))
-ans = input("Please confirm [Y/n]:")
-if ans.lower() == 'n':
-    sys.exit(0)
-
-# load the album MP3
-album_stream = ffmpeg.input(album_mp3_path).audio
-
-# iterate through tracklist
-track_count = len(tracklist)
-for i, track_info in enumerate(tracklist):
-
-    # get data fields from track info
-    track_number = i + 1
-    track_start = track_info["start_time"]
-    if i < (track_count - 1):
-        track_end = tracklist[i + 1]["start_time"]
-    else:
-        track_end = None
-    track_title = track_info["title"]
-
-    # TODO Use bitrate of the input file. We now set the bitrate explicitly, because it
-    #      was reduced from original 160k to 128k, for some reason.
-
-    # write output stream
-    track_filename = f"{track_number:02d} {track_title}.mp3"
-    track_path = track_filename
-    if not (os.path.exists(track_path) and SKIP_EXISTING_TRACKS):
-        if not track_end is None:
-            track_stream = ffmpeg.output(album_stream, track_path, audio_bitrate="160k",
-                ss=track_start, to=track_end)
+        mp3_list = glob.glob('*.mp3')
+        if len(mp3_list) > 0:
+            album_mp3_path = mp3_list[0]
+            print(f"WARNING: No MP3 file specified, using '{album_mp3_path}'.")
         else:
-            track_stream = ffmpeg.output(album_stream, track_path, audio_bitrate="160k",
-                ss=track_start)
-        ffmpeg.run(track_stream)
+            print('ERROR: No MP3 file specified, none found.')
+            sys.exit(1)
+        txt_list = glob.glob('*.txt')
+        if len(txt_list) > 0:
+            tracklist_path = txt_list[0]
+            print(f"WARNING: No tracklist specified, using '{tracklist_path}'")
+        else:
+            print('ERROR: No tracklist specified, none found.')
+            sys.exit(1)
+    
+    return (album_mp3_path, tracklist_path)
 
-    # write ID3 tags
-    id3 = EasyID3(track_path)
-    id3["tracknumber"] = f"{track_number:02d}"
-    id3["title"] = track_title
-    if 'artist' in track_info:
-        id3['artist'] = track_info['artist']
-    id3.save()
 
-    # stop after X tracks (to save time while debugging)
-    if (STOP_AFTER_X_TRACKS is not None) and (i == (STOP_AFTER_X_TRACKS - 1)):
-        break
+def parse_tracklist(path):
+
+    # read lines from tracklist text file
+    with open(path, "rt", encoding="utf8") as fobj:
+        lines = fobj.readlines()
+
+    # ask for selection of tracklist format
+    print(f'\nFirst line of provided tracklist:')
+    print(lines[0])
+    print('Supported tracklist formats:')
+    trfl_len = len(TRACK_FORMATS)
+    for i in range(trfl_len):
+        trf = TRACK_FORMATS[i]
+        print(f'{i + 1}:   {trf[0]}')
+    ans = int(input('Select format (1-4): '))
+    selected_trf = TRACK_FORMATS[int(ans) - 1]
+
+    # parse the line with a regex, to get track info
+    trf_regex = selected_trf[1]
+    trf_fields = selected_trf[2]
+    regex = re.compile(trf_regex)
+    tracklist = []
+    for line in lines:
+        match = regex.match(line)
+        track_info = {}
+        for i in range(len(trf_fields)):
+            track_info[trf_fields[i]] = match.group(i+1)
+        tracklist.append(track_info)
+
+    # ask for confirmation of parsed tracklist
+    print("Tracklist has been parsed to following items:")
+    print('\n'.join([str(x) for x in tracklist]))
+    ans = input("Please confirm [Y/n]:")
+    if ans.lower() == 'n':
+        sys.exit(0)
+    
+    return tracklist
+
+
+def split_album(path, tracklist):
+
+    # load the album MP3
+    album_stream = ffmpeg.input(path).audio
+
+    # iterate through tracklist
+    track_count = len(tracklist)
+    for i, track_info in enumerate(tracklist):
+
+        # get data fields from track info
+        track_number = i + 1
+        track_start = track_info["start_time"]
+        if i < (track_count - 1):
+            track_end = tracklist[i + 1]["start_time"]
+        else:
+            track_end = None
+        track_title = track_info["title"]
+
+        # TODO Use bitrate of the input file. We now set the bitrate explicitly, because it
+        #      was reduced from original 160k to 128k, for some reason.
+
+        # write output stream
+        track_filename = f"{track_number:02d} {track_title}.mp3"
+        track_path = track_filename
+        if not (os.path.exists(track_path) and SKIP_EXISTING_TRACKS):
+            if not track_end is None:
+                track_stream = ffmpeg.output(album_stream, track_path, audio_bitrate="160k",
+                    ss=track_start, to=track_end)
+            else:
+                track_stream = ffmpeg.output(album_stream, track_path, audio_bitrate="160k",
+                    ss=track_start)
+            ffmpeg.run(track_stream)
+
+        # write ID3 tags
+        id3 = EasyID3(track_path)
+        id3["tracknumber"] = f"{track_number:02d}"
+        id3["title"] = track_title
+        if 'artist' in track_info:
+            id3['artist'] = track_info['artist']
+        id3.save()
+
+        # stop after X tracks (to save time while debugging)
+        if (STOP_AFTER_X_TRACKS is not None) and (i == (STOP_AFTER_X_TRACKS - 1)):
+            break
+
+#---------------------------------------------------------------------------------------------------
+# Script Body
+#---------------------------------------------------------------------------------------------------
+
+def main():
+    (al_path, tl_path) = get_input_filenames()
+    tracklist = parse_tracklist(tl_path)
+    split_album(al_path, tracklist)
+
+if __name__ == '__main__':
+    main()
